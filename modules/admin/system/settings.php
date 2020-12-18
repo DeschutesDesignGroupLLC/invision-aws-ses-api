f0849aaadcd4f37cc8cf5017304360a5774105e1
Jon Erickson <jon@deschutesdesigngroup.com>
2020-12-18 07:07:02 -0800

    Encrypt secret key.

--- modules/admin/system/settings.php
+++ modules/admin/system/settings.php
@@ -43,13 +43,19 @@ class _settings extends \IPS\Dispatcher\Controller
 		// Create a new form
 		$form = new \IPS\Helpers\Form;
 
+		// Decrypt the secret key
+		$secret = NULL;
+		if (\IPS\Settings::i()->awsses_secret_key) {
+			$secret = \IPS\Text\Encrypt::fromTag(\IPS\Settings::i()->awsses_secret_key)->decrypt();
+		}
+
 		// Add settings
 		$form->addHeader(\IPS\Member::loggedIn()->language()->addToStack('awsses_settings_header'));
 		$form->addMessage('awsses_settings_email_override_message', 'ipsPad ipsMessage ipsMessage_warning ipsType_reset ipsSpacer_top');
 		$form->addMessage('awsses_settings_message');
 		$form->add( new \IPS\Helpers\Form\YesNo( 'awsses_enabled', \IPS\Settings::i()->awsses_enabled, TRUE ) );
 		$form->add( new \IPS\Helpers\Form\Text( 'awsses_access_key', \IPS\Settings::i()->awsses_access_key, TRUE ) );
-		$form->add( new \IPS\Helpers\Form\Password( 'awsses_secret_key', \IPS\Settings::i()->awsses_secret_key, TRUE ) );
+		$form->add( new \IPS\Helpers\Form\Password( 'awsses_secret_key', $secret, TRUE ) );
 		$form->add( new \IPS\Helpers\Form\Text( 'awsses_region', \IPS\Settings::i()->awsses_region, TRUE, array( 'placeholder' => 'us-west-2', '' ) ) );
 		$form->add( new \IPS\Helpers\Form\Text( 'awsses_config_set_name', \IPS\Settings::i()->awsses_config_set_name, FALSE ) );
 
@@ -57,7 +63,12 @@ class _settings extends \IPS\Dispatcher\Controller
 		if ( $values = $form->values() )
 		{
 			// Log
-			\IPS\Session::i()->log( 'awsses_settings_updated' );
+			\IPS\Session::i()->log('awsses_settings_updated');
+
+			// Encrypt the secret key
+			if ($values['awsses_secret_key']) {
+				$values['awsses_secret_key'] = \IPS\Text\Encrypt::fromPlaintext($values['awsses_secret_key'])->tag();
+			}
 
 			// Save the settings
 			$form->saveAsSettings();
