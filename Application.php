65438e594602b931ce9190b2150a4ebf5b641160
Jon Erickson <jon@deschutesdesigngroup.com>
2022-07-07 21:24:02 -0700

    Migrated dev environment to docker and added condition to load AWS library if it hasn't already been loaded.

--- Application.php
+++ Application.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief		AWS Simple Email Service Application Class
  * @author		<a href='https://www.deschutesdesigngroup.com'>Deschutes Design Group LLC</a>
@@ -21,9 +22,11 @@ class _Application extends \IPS\Application
 	 */
 	public function __construct()
 	{
-		// Load vendor library
-		require_once static::getRootPath() . '/applications/awsses/sources/vendor/autoload.php';
-	}
+		// Load vendor library if it hasn't already been
+        if (!class_exists('Aws\\Ses\\SesClient') || !class_exists('Aws\\Sns\\SnsClient')) {
+            require_once static::getRootPath() . '/applications/awsses/sources/vendor/autoload.php';
+        }
+    }
 
 	/**
 	 * [Node] Get Icon for tree
